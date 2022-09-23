92f9f3470afca31cc801f9a60ab2d194e4091a0a
Zychronix <zychronix1@gmail.com>
2022-09-23 00:46:40 -0400

    yeehaw

---
+++ ReLoDed Launcher/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ReLoDed_Launcher
+{
+    partial class ReLoDed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReLoDed));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtxtInfoBox = new System.Windows.Forms.RichTextBox();
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pgrTask = new System.Windows.Forms.ProgressBar();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Legend of Dragoon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ReLoDed/Severed Chains";
+            // 
+            // rtxtInfoBox
+            // 
+            this.rtxtInfoBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.rtxtInfoBox.ForeColor = System.Drawing.Color.White;
+            this.rtxtInfoBox.Location = new System.Drawing.Point(10, 85);
+            this.rtxtInfoBox.Name = "rtxtInfoBox";
+            this.rtxtInfoBox.Size = new System.Drawing.Size(570, 355);
+            this.rtxtInfoBox.TabIndex = 2;
+            this.rtxtInfoBox.Text = "";
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnLaunch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLaunch.ForeColor = System.Drawing.Color.White;
+            this.btnLaunch.Location = new System.Drawing.Point(587, 408);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(188, 36);
+            this.btnLaunch.TabIndex = 3;
+            this.btnLaunch.Text = "Launch";
+            this.btnLaunch.UseVisualStyleBackColor = false;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(657, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tools";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(618, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Coming Soon™";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(587, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Import Save Files";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(587, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Mod Manager";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(587, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(587, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(188, 36);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button6.Enabled = false;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(587, 285);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(188, 36);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // pgrTask
+            // 
+            this.pgrTask.Location = new System.Drawing.Point(588, 385);
+            this.pgrTask.Name = "pgrTask";
+            this.pgrTask.Size = new System.Drawing.Size(186, 23);
+            this.pgrTask.TabIndex = 8;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button7.Enabled = false;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(587, 335);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(188, 36);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "...";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // ReLoDed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 456);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pgrTask);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLaunch);
+            this.Controls.Add(this.rtxtInfoBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ReLoDed";
+            this.Text = "ReLoDed";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxtInfoBox;
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ProgressBar pgrTask;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ ReLoDed Launcher/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReLoDed_Launcher
+{
+    public partial class ReLoDed : Form
+    {
+        string[] fileChecks = {
+            "isos/1.iso",
+            "isos/2.iso",
+            "isos/3.iso",
+            "isos/4.iso"
+        };
+
+        public ReLoDed()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            if (CheckFiles())
+            {
+                if (CheckISO())
+                {
+                    if (GetJavaVersion())
+                    {
+                        try
+                        {
+                            string jarName = "";
+
+                            foreach (string fileName in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jar", SearchOption.TopDirectoryOnly))
+                            {
+                                if (fileName.Contains("lod-game"))
+                                    jarName = Path.GetFileName(fileName);
+                            }
+                            if (jarName.Equals(""))
+                            {
+                                throw new Exception("Incorrect JAR name or JAR not found.");
+                            }
+                            Process pr = new Process();
+                            ProcessStartInfo psi = new ProcessStartInfo();
+                            psi.FileName = "cmd.exe";
+                            psi.Arguments = @"/K java -cp " + jarName + ";libs/* legend.game.MainWindows -ea"; 
+                            pr.StartInfo = psi;
+                            pr.Start();
+                            pr.WaitForExit();
+                        }
+                        catch (Exception ex)
+                        {
+                            rtxtInfoBox.Text += "Something went wrong launching the game";
+                            rtxtInfoBox.Text += Environment.NewLine;
+
+                            rtxtInfoBox.Text += ex.ToString();
+                            rtxtInfoBox.Text += Environment.NewLine;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private bool CheckFiles()
+        {
+            bool pass = true;
+            string errorBuilder = "You are missing the following files needed to run the game.";
+
+            for (int i = 0; i < fileChecks.Length; i++)
+            {
+                if (!File.Exists(fileChecks[i]))
+                {
+                    pass = false;
+                    errorBuilder += "\r\n" + fileChecks[i];
+                }
+            }
+
+            if (!pass)
+            {
+                rtxtInfoBox.Text += errorBuilder;
+                rtxtInfoBox.Text += Environment.NewLine;
+            }
+            return pass;
+        }
+
+        private bool CheckISO()
+        {
+            bool pass = true;
+            string errorBuilder = "Something is wrong with the following ISOs.\r\n";
+            try
+            {
+
+                for (int i = 1; i < 5; i++)
+                {
+                    byte[] playstation = new byte[0xB];
+                    byte[] region = new byte[0xA];
+                    byte[] disc = new byte[0x5];
+
+                    using (BinaryReader reader = new BinaryReader(File.Open($"isos\\{i}.iso", FileMode.Open)))
+                    {
+                        reader.BaseStream.Seek(0x9320, SeekOrigin.Begin);
+                        reader.Read(playstation, 0, 0xB);
+                        reader.BaseStream.Seek(0x15, SeekOrigin.Current);
+                        reader.Read(region, 0, 0xA);
+                        reader.BaseStream.Seek(0x8C, SeekOrigin.Current);
+                        reader.Read(disc, 0, 0x5);
+                    }
+
+                    errorBuilder += $"Disc {i}\r\n";
+
+                    if (!System.Text.Encoding.Default.GetString(playstation).Equals("PLAYSTATION"))
+                    {
+                        errorBuilder += "Not a PLAYSTATION disc.\r\n";
+                        pass = false;
+                    }
+
+                    if (!System.Text.Encoding.Default.GetString(region).Contains("SCUS"))
+                    {
+                        errorBuilder += "ReLoDed currently only supports NA Region.\r\n";
+                        pass = false;
+                    }
+
+                    int discCheck = Convert.ToInt32(System.Text.Encoding.Default.GetString(disc).Substring(4, 1));
+                    if (discCheck != i)
+                    {
+                        errorBuilder += $"Your disc {i} is in the wrong order {discCheck}.\r\n";
+                        pass = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                pass = false;
+                errorBuilder = "Something went wrong reading the discs or ReLoDed is already open.";
+            }
+
+            if (!pass)
+            {
+                rtxtInfoBox.Text += errorBuilder;
+                rtxtInfoBox.Text += Environment.NewLine;
+            }
+            return pass;
+        }
+
+        private bool GetJavaVersion()
+        {
+            bool pass = true;
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "java.exe";
+                psi.Arguments = " -version";
+                psi.RedirectStandardError = true;
+                psi.UseShellExecute = false;
+
+                Process pr = Process.Start(psi);
+                string output = pr.StandardError.ReadLine();
+                output = output.Replace("\"", "");
+                if (Int32.Parse(output.Split(' ')[2].Substring(0, 2)) < 17)
+                {
+                    pass = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                rtxtInfoBox.Text += "Open JDK version 17 is needed to run ReLoDed.";
+                rtxtInfoBox.Text += Environment.NewLine;
+            }
+            return pass;
+        }
+    }
+}
